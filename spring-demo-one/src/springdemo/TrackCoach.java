d94f502c207d8cf4b87108a0ea97ed543a578977
czahie <czahie1996@gmail.com>
2019-06-20 22:30:22 +0800

    add scope and life cycle codes

--- spring-demo-one/src/springdemo/TrackCoach.java
+++ spring-demo-one/src/springdemo/TrackCoach.java
@@ -22,5 +22,16 @@ public class TrackCoach implements Coach {
 	public String getDailyFortune() {
 		return "Just Do It: " + fortuneService.getFortune();
 	}
+	
+	// add an init method 
+	public void doMyStartupMethod() {
+		System.out.println("TrackCoach: inside method doMyStartupMethod");
+	}
+	
+	// add a destroy method
+	public void doMyCleanupMethod() {
+		System.out.println("TrackCoach: inside method doMyCleanupMethod");
+	}
+	
 
 }
