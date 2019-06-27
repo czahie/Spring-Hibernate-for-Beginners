6d7d3666a67cc92521ea2fdd0826f6044cded817
czahie <czahie1996@gmail.com>
2019-06-27 23:11:00 +0800

    Form tags

--- spring-mvc-demo/WebContent/WEB-INF/view/main-menu.jsp
+++ spring-mvc-demo/WebContent/WEB-INF/view/main-menu.jsp
@@ -2,11 +2,6 @@
 <html>
 
 <head>
-
-    <link rel="stylesheet" type="text/css”
-          href="${pageContext.request.contextPath}/resources/css/my-test.css">
-          
-    <script src="${pageContext.request.contextPath}/resources/js/simple-test.js"></script>
           
 </head>
 
@@ -20,11 +15,7 @@
 
 <br><br>
 
-<img src="${pageContext.request.contextPath}/resources/images/spring-logo.png" />
-
-<br><br>
-
-<input type="button" onclick="doSomeWork()" value="Click Me"/>
+<a href="student/showForm">Student Form</a>
 
 </body>
 
---
+++ spring-mvc-demo/src/springdemo/mvc/Student.java
@@ -0,0 +1,55 @@
+package springdemo.mvc;
+
+import java.util.LinkedHashMap;
+
+public class Student {
+
+	// fields
+	private String firstName;
+	private String lastName;
+	private String country;
+	private LinkedHashMap<String, String> countryOptions;
+
+	public Student() {
+		
+		// populate country options: use ISO code 
+		countryOptions = new LinkedHashMap<>();
+		
+		countryOptions.put("CN", "China");
+		countryOptions.put("JP", "Japan");
+		countryOptions.put("KR", "Korean");
+		countryOptions.put("IR", "Iran");
+		countryOptions.put("VT", "Vietnam");
+		countryOptions.put("US", "United States of America");
+	}
+
+	// getters and setters 
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+	
+	public String getCountry() {
+		return country;
+	}
+
+	public void setCountry(String country) {
+		this.country = country;
+	}
+
+	public LinkedHashMap<String, String> getCountryOptions() {
+		return countryOptions;
+	}
+
+}
