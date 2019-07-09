6d874b0a4332751706ccf77d0acbde64a6eb5996
czahie <czahie1996@gmail.com>
2019-07-09 23:16:09 +0800

    hibernate

---
+++ hibernate-tutorial/src/hibernate/demo/PrimaryKeyDemo.java
@@ -0,0 +1,48 @@
+package hibernate.demo;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+import hibernate.demo.entity.Student;
+
+public class PrimaryKeyDemo {
+
+	public static void main(String[] args) {
+
+		// create session factory
+		SessionFactory factory = new Configuration()
+								.configure("hibernate.cfg.xml")
+								.addAnnotatedClass(Student.class)
+								.buildSessionFactory();
+		
+		// create session
+		Session session = factory.getCurrentSession();
+		
+		try {			
+			// create three student objects
+			System.out.println("Creating 3 student object...");
+			Student tempStudent1 = new Student("Joe", "Joe", "Joe@gmail.com");
+			Student tempStudent2 = new Student("James", "Harden", "jh@gmail.com");
+			Student tempStudent3 = new Student("Chris", "Paul", "cp3@gmail.com");
+			
+			// start a transaction
+			session.beginTransaction();
+			
+			// save the student object
+			System.out.println("Saving the students...");
+			session.save(tempStudent1);
+			session.save(tempStudent2);
+			session.save(tempStudent3);
+			
+			// commit transaction
+			session.getTransaction().commit();
+			
+			System.out.println("Done!");
+		}
+		finally {
+			factory.close();
+		}
+	}
+
+}
