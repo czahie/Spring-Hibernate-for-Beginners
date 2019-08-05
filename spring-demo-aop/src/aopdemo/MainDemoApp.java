96d5e5d0c442002e8f7f7fbb8fbf65cc455e3ac6
czahie <czahie1996@gmail.com>
2019-08-05 22:54:25 +0800

    spring aop demo

---
+++ spring-demo-aop/src/aopdemo/MainDemoApp.java
@@ -0,0 +1,39 @@
+package aopdemo;
+
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+import aopdemo.dao.AccountDAO;
+import aopdemo.dao.MembershipDAO;
+
+public class MainDemoApp {
+
+	public static void main(String[] args) {
+		
+		// read spring config java class
+		AnnotationConfigApplicationContext context = 
+				new AnnotationConfigApplicationContext(DemoConfig.class);
+		
+		// get the bean from spring container
+		AccountDAO theAccountDAO = context.getBean("accountDAO", AccountDAO.class);
+		
+		// get membership bean from spring container
+		MembershipDAO theMembershipDAO = context.getBean("membershipDAO", MembershipDAO.class);
+		
+		// call the business method
+		theAccountDAO.addAccount();
+		
+		// call the membership business method
+		theMembershipDAO.addSillyMember();
+		
+//		// do it again
+//		System.out.println("\nLet's call it again!\n");
+//		
+//		// call the business method again
+//		theAccountDAO.addAccount();
+		
+		// close the context
+		context.close();
+
+	}
+
+}
